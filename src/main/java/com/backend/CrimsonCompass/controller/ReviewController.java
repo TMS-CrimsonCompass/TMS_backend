33dd384c9b9f9bbde7d65b47ff23f06ce3e4a819
Preetham Reddy <preethamreddy0206@gmail.com>
2025-04-28 17:43:09 -0400

    review controller changes

--- src/main/java/com/backend/CrimsonCompass/controller/ReviewController.java
+++ src/main/java/com/backend/CrimsonCompass/controller/ReviewController.java
@@ -2,10 +2,16 @@ package com.backend.CrimsonCompass.controller;
 
 import com.backend.CrimsonCompass.dto.ReviewRequestDTO;
 import com.backend.CrimsonCompass.dto.ReviewResponseDTO;
+import com.backend.CrimsonCompass.security.JwtProperties;
 import com.backend.CrimsonCompass.service.IReviewService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
+import javax.crypto.SecretKey;
 import java.util.List;
 
 @RestController
@@ -14,13 +20,29 @@ public class ReviewController {
 
     private final IReviewService reviewService;
 
+    private final SecretKey jwtSecretKey;
+    private final JwtProperties jwtProperties;
+
     @Autowired
-    public ReviewController(IReviewService reviewService) {
+    public ReviewController(IReviewService reviewService, SecretKey jwtSecretKey, JwtProperties jwtProperties) {
         this.reviewService = reviewService;
+        this.jwtSecretKey = jwtSecretKey;
+        this.jwtProperties = jwtProperties;
     }
 
     @PostMapping
-    public void addReview(@RequestBody ReviewRequestDTO reviewRequestDTO) {
+    public void addReview(@RequestBody ReviewRequestDTO reviewRequestDTO, HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(jwtSecretKey)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+            Integer userId = (Integer) claims.get("userId");
+            reviewRequestDTO.setUserId(Long.valueOf(userId));
+        }
         reviewService.addReview(reviewRequestDTO);
     }
 
